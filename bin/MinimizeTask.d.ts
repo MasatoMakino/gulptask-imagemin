ca1f712c4e81cb8dfe9c6ec5fa8ba35b23d35f8e
MasatoMakino <makino.masato.g@gmail.com>
2022-06-05 12:13:56 +0900

    remove : bin
    tscで生成されたjsはnpmのみで公開する。gitでは管理しない。

--- bin/MinimizeTask.d.ts
+++
@@ -1,8 +0,0 @@
-import { ScaleOption } from "./Option";
-/**
- * .imgBufferディレクトリの更新差分を抽出して画像を最適化する
- */
-export declare function getNewerFileOptimizeTask(imageDir: string, srcImageGlob: string, distImgPath: string, extensions: string[], resizeOption?: ScaleOption): Promise<any[]>;
-export declare function getScalingTask(imageDir: string, scaleOption: ScaleOption): Promise<any[]>;
-export declare function getWatchImages(imageDir: string, distDir: string, scaleOptions: ScaleOption[]): () => void;
-//# sourceMappingURL=MinimizeTask.d.ts.map
